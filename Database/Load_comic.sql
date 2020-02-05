677e9a21c7971c650827bd66a84178ec14040299
Kingpin2099 <51490106+Kingpin2099@users.noreply.github.com>
2020-02-05 08:11:24 -0700

    Add files via upload

---
+++ Database/Load_comic.sql
@@ -0,0 +1,22 @@
+/*insert into Comic (itemID,author,volume,isbn,publisher)
+	values (0000000000,'',,'','');*/
+	insert into Comic (itemID, author, volume, isbn, publisher)
+	values (0000000001, 'Allan Moore', 01,  '9781401216672', 'DC');
+	insert into Comic (itemID, author, volume, isbn, publisher)
+	values (0000000002, 'Jason Aaron', 01,  '9781302921026', 'Marvel');
+	insert into Comic (itemID, author, volume, isbn, publisher)
+	values (0000000003, 'Kieron Gillen', 01 , '9781684154913', 'BOOM! Studios');
+	insert into Comic (itemID, author, volume, isbn, publisher)
+	values (0000000004, 'Rodney Barnes',01 ,'9781534315693','Image Comics');
+	insert into Comic (itemID, author, volume, isbn, publisher)
+	values (0000000005, 'Scott Snyder', 01,'9781401294960', 'DC');
+	insert into Comic (itemID, author, volume, isbn, publisher)
+	values (0000000006, 'Tom Taylor',01 ,'9781401294403', 'DC');
+	insert into Comic (itemID, author, volume, isbn, publisher)
+	values (0000000007, 'Leah Williams',01 ,'9781302915780','Marvel');
+	insert into Comic (itemID, author, volume, isbn, publisher)
+	values (0000000008, 'Gabriel Hardman',02,'9781401293031','DC');
+	insert into Comic (itemID, author, volume, isbn, publisher)
+	values (0000000009,'Al Ewing',05,'9781302916688','Marvel');
+	insert into Comic (itemID, author, volume, isbn, publisher)
+	values (0000000010,'Peter David',01,'9781302904364','Marvel');
---
+++ Database/Load_vendor.sql
@@ -0,0 +1,19 @@
+/*insert into vendor (vendorID,vendor_name)
+	values (0000000000,'');*/
+insert into vendor (vendorID,vendor_name)
+	values (1234567890,'Comics Inc.');
+insert into vendor (vendorID,vendor_name)
+	values (2190293874,'Unique Comics ltd.');
+insert into vendor (vendorID,vendor_name)
+	values (0005478965,'CocaCola');
+insert into vendor (vendorID,vendor_name)
+	values (0005478966,'Pepsi');
+insert into vendor (vendorID,vendor_name)
+	values (00007896541,'Funko');
+insert into vendor (vendorID,vendor_name)
+	values (0005478935,'Acme foods');
+insert into vendor (vendorID,vendor_name)
+	values (0045844571,'Marvel Toys');
+insert into vendor (vendorID,vendor_name)
+	values (0045844573,'DC Toys');
+		
