0e967fb3bb56b88a0725ac253cd5021196c72e61
Kingpin2099 <51490106+Kingpin2099@users.noreply.github.com>
2020-02-06 11:28:51 -0700

    Initial upload (only i column.. needs changes later)

---
+++ Database/Load_order.sql
@@ -0,0 +1,19 @@
+/*insert into Order (order_no)
+	values (0000);*/
+insert into Order (order_no)
+	values (0001);
+insert into Order (order_no)
+	values (0002);
+insert into Order (order_no)
+	values (0003);
+insert into Order (order_no)
+	values (0004);
+insert into Order (order_no)
+	values (0005);
+insert into Order (order_no)
+	values (0006);
+insert into Order (order_no)
+	values (0007);
+insert into Order (order_no)
+	values (0008);
+									
