3abdae6df6a29a1e087a7b0cb6f5380ebdcf4d7c
Kingpin2099 <51490106+Kingpin2099@users.noreply.github.com>
2020-02-11 11:20:06 -0700

    finished the load scripts also changed item ID
    itemID still a 10 digit interger but i took off most of the leading 00's

--- Database/Load_OrderItem.sql
+++ Database/Load_OrderItem.sql
@@ -1,50 +1,50 @@
 /*insert into OrderItem (itemID,order_no,purchase_price)
 	values (0000,0000,00.00);*/
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000001,0001,8.50);
+	values (001,0001,8.50);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000002,0001,5.50);
+	values (002,0001,5.50);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000003,0002,4.75);
+	values (003,0002,4.75);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000004,0002,3.45);
+	values (004,0002,3.45);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000005,0001,10.99);
+	values (005,0001,10.99);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000006,0001,12.99);
+	values (006,0001,12.99);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000007,0001,6.25);
+	values (007,0001,6.25);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000008,0001,12.75);
+	values (008,0001,12.75);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000009,0001,5.25);
+	values (009,0001,5.25);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000010,0001,10.18);
+	values (010,0001,10.18);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000011,0003,00.50);
+	values (011,0003,00.50);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000012,0004,00.50);
+	values (012,0004,00.50);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000013,0005,5.75);
+	values (013,0005,5.75);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000014,0005,5.75);
+	values (014,0005,5.75);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000015,0005,5.75);
+	values (015,0005,5.75);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000016,0005,5.75);
+	values (016,0005,5.75);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000017,0006,0.25);
+	values (017,0006,0.25);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000018,0006,0.25);
+	values (018,0006,0.25);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000019,0006,00.40);
+	values (019,0006,00.40);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000020,0006,00.40);
+	values (020,0006,00.40);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000021,0007,6.00);
+	values (021,0007,6.00);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000022,0007, 6.00);
+	values (022,0007, 6.00);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000023,0008,5.60);
+	values (023,0008,5.60);
 insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000024,0008,5.60);															
+	values (024,0008,5.60);															
--- Database/Load_comic.sql
+++ Database/Load_comic.sql
@@ -1,22 +1,22 @@
 /*insert into Comic (itemID,author,volume,isbn,publisher)
 	values (0000000000,'',,'','');*/
 	insert into Comic (itemID, author, volume, isbn, publisher)
-	values (0000000001, 'Allan Moore', 01,  '9781401216672', 'DC');
+	values (001, 'Allan Moore', 01,  '9781401216672', 'DC');
 	insert into Comic (itemID, author, volume, isbn, publisher)
-	values (0000000002, 'Jason Aaron', 01,  '9781302921026', 'Marvel');
+	values (002, 'Jason Aaron', 01,  '9781302921026', 'Marvel');
 	insert into Comic (itemID, author, volume, isbn, publisher)
-	values (0000000003, 'Kieron Gillen', 01 , '9781684154913', 'BOOM! Studios');
+	values (003, 'Kieron Gillen', 01 , '9781684154913', 'BOOM! Studios');
 	insert into Comic (itemID, author, volume, isbn, publisher)
-	values (0000000004, 'Rodney Barnes',01 ,'9781534315693','Image Comics');
+	values (004, 'Rodney Barnes',01 ,'9781534315693','Image Comics');
 	insert into Comic (itemID, author, volume, isbn, publisher)
-	values (0000000005, 'Scott Snyder', 01,'9781401294960', 'DC');
+	values (005, 'Scott Snyder', 01,'9781401294960', 'DC');
 	insert into Comic (itemID, author, volume, isbn, publisher)
-	values (0000000006, 'Tom Taylor',01 ,'9781401294403', 'DC');
+	values (006, 'Tom Taylor',01 ,'9781401294403', 'DC');
 	insert into Comic (itemID, author, volume, isbn, publisher)
-	values (0000000007, 'Leah Williams',01 ,'9781302915780','Marvel');
+	values (007, 'Leah Williams',01 ,'9781302915780','Marvel');
 	insert into Comic (itemID, author, volume, isbn, publisher)
-	values (0000000008, 'Gabriel Hardman',02,'9781401293031','DC');
+	values (008, 'Gabriel Hardman',02,'9781401293031','DC');
 	insert into Comic (itemID, author, volume, isbn, publisher)
-	values (0000000009,'Al Ewing',05,'9781302916688','Marvel');
+	values (009,'Al Ewing',05,'9781302916688','Marvel');
 	insert into Comic (itemID, author, volume, isbn, publisher)
-	values (0000000010,'Peter David',01,'9781302904364','Marvel');
+	values (010,'Peter David',01,'9781302904364','Marvel');
