414b6b6754d4def2e42196f732a3a5ee693e6586
Kingpin2099 <51490106+Kingpin2099@users.noreply.github.com>
2020-02-27 11:03:44 -0700

    changed to fit revised erd

---
+++ Database/Load_orders.sql
@@ -0,0 +1,19 @@
+/*insert into Orders (order_no)
+	values (0000,2020-01-19);*/
+insert into Orders (order_no, orderDate, arrivalDate, VendorID)
+	values (0001,2019-12-10,2020-01-10,1);
+insert into Orders (order_no, orderDate, arrivalDate, VendorID)
+	values (0002,2019-12-10,2019-01-10,2);
+insert into Orders (order_no, orderDate, arrivalDate, VendorID)
+	values (0003,2019-12-08,2019-01-12,3);
+insert into Orders (order_no, orderDate, arrivalDate, VendorID)
+	values (0004,2019-12-07,2019-01-12,4);
+insert into Orders (order_no, orderDate, arrivalDate, VendorID)
+	values (0005,2019-12-05,2019-01-12,5);
+insert into Orders (order_no, orderDate, arrivalDate, VendorID)
+	values (0006,2019-12-05,2019-01-15,6);
+insert into Orders (order_no, orderDate, arrivalDate, VendorID)
+	values (0007,2019-12-03,2019-01-15,7);
+insert into Orders (order_no, orderDate, arrivalDate, VendorID)
+	values (0008,2019-12-01,2019-01-15,8);
+									
