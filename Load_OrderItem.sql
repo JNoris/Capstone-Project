052b9909e3288ab4c27e1a89d84c59e0386d0597
Kingpin2099 <51490106+Kingpin2099@users.noreply.github.com>
2020-02-27 11:02:35 -0700

    Changed to fit the revised ERD

--- Load_OrderItem.sql
+++ Load_OrderItem.sql
@@ -1,51 +1,50 @@
-/*insert into OrderItem (itemID,order_no,purchase_price)
+/*insert into Order_Item (itemID,order_no,purchase_price)
 	values (0000,0000,00.00);*/
-	/**/
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000001,0001,8.50);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000002,0001,5.50);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000003,0002,4.75);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000004,0002,3.45);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000005,0001,10.99);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000006,0001,12.99);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000007,0001,6.25);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000008,0001,12.75);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000009,0001,5.25);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000010,0001,10.18);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000011,0003,00.50);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000012,0004,00.50);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000013,0005,5.75);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000014,0005,5.75);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000015,0005,5.75);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000016,0005,5.75);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000017,0006,0.25);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000018,0006,0.25);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000019,0006,00.40);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000020,0006,00.40);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000021,0007,6.00);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000022,0007, 6.00);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000023,0008,5.60);
-insert into OrderItem (itemID,order_no,purchase_price)
-	values (0000000024,0008,5.60);															
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (001,0001,8.50,5);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (002,0001,5.50,10);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (003,0002,4.75,3);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (004,0002,3.45,8);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (005,0001,10.99,2);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (006,0001,12.99,4);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (007,0001,6.25,1);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (008,0001,12.75,2);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (009,0001,5.25,6);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (010,0001,10.18,7);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (011,0003,00.50,160);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (012,0004,00.50,280);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (013,0005,5.75,10);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (014,0005,5.75,6);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (015,0005,5.75,3);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (016,0005,5.75,5);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (017,0006,0.25,18);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (018,0006,0.25,22);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (019,0006,00.40,20);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (020,0006,00.40,20);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (021,0007,6.00,5);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (022,0007, 6.00,2);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (023,0008,5.60,5);
+insert into Order_Item (itemID,order_no,purchase_price,orderQuantity)
+	values (024,0008,5.60,5);															
