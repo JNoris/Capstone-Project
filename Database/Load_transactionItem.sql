f4abdc847db1330830c8e740389db26cf0d1cbbc
skruey <sskrudland@hotmail.com>
2020-03-03 10:22:31 -0700

    Finished master load script

--- Database/Load_transactionItem.sql
+++ Database/Load_transactionItem.sql
@@ -1,40 +1,40 @@
 /*insert into TransactionItem (itemID,transactionID,price)
 	values (000,00,00.00);*/
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (001,01,15.99);
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (008,01,33.65);
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (015,01,8.49);	
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (011,01,0.99);
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (012,02,0.99);
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (022,03,12.75);
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (023,03,11.25);
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (005,03,39.59);
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (001,04,15.99);
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (009,04,19.18);
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (017,05,1.50);
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (011,06,0.99);
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (020,06,1.25);
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (003,07,16.99);
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (010,07,39.64);
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (009,07,19.18);
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (004,07,9.99);
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (014,07,29.00);
-insert into TransactionItem (itemID,transactionID,price)
+insert into Transaction_Item (itemID,transactionID,price)
 	values (021,07,12.75);																		
