ed7cdd991f9007fc6557ed291a61dd83d3ebf2f7
skruey <sskrudland@hotmail.com>
2020-03-31 13:54:55 -0600

    Revert Commit

--- Database/Load_Item.sql
+++ Database/Load_Item.sql
@@ -1,50 +1,52 @@
-/*insert into Item (itemID,item_typen,price,name,quantity,upc)
+
+/*insert into Item (itemID,item_type,vendorID,description,price,name,quantity,upc)
 	values (000,'',,null,,'',,);*/
-insert into Item (itemID, item_type,description, price, name, quantity, upc)
-	values (001, 'comic',null, 15.99, 'Batman: The Killing Joke', 5, 82341785611092745);
-insert into Item (itemID, item_type,description, price, name, quantity, upc)
-	values (002, 'comic','King Thor volume 1', 10.99, 'King Thor', 10, 32904867028978325);
-insert into Item (itemID, item_type,description, price, name, quantity, upc)
-	values (003, 'comic','Once and Future volume 1', 16.99, 'Once and Future',3,094785619542856347 );
-insert into Item (itemID, item_type,description, price, name, quantity, upc)
-	values (004, 'comic','Killadelphia Volume 1: Sins of the Father', 9.99, 'Killadelphia',8, 96874325412358974 );
-insert into Item (itemID, item_type,description,price, name, quantity, upc)
-	values (005, 'comic','Batman: Last Knight on Earth Hardcover', 39.59 , 'Batman: Last Knight on Earth',2,788552147963258 );
-insert into Item (itemID, item_type,description, price, name, quantity, upc)
-	values (006, 'comic','DCeased Hardcover',34.15, 'DCeased',4, 96874325415438974);
-insert into Item (itemID, item_type,description, price, name, quantity, upc)
-	values (007, 'comic','Age of X-Man: X-Tremists compilation',19.17 ,'Age of X-Man: X-Tremists',1,45887741258963014);
-insert into Item (itemID, item_type,description, price, name, quantity, upc)
-	values (008, 'comic','Green Lantern: Earth One Vol. 2 Hardcover',33.65,'Green Lantern: Earth One Vol. 2',2,85236974132145698);
-insert into Item (itemID, item_type,description, price, name, quantity, upc)
-	values (009, 'comic','Immortal Hulk Vol. 5: Breaker of Worlds',19.18,'Immortal Hulk Vol. 5',6,96374185245695175);
-insert into Item (itemID, item_type,description, price, name, quantity, upc)
-	values (010,'comic','FN Spider-Man Complete',39.64,'Friendly Neighborhood Spider-Man',7,5236478910304 );
-insert into Item (itemID,item_type,description,price,name,quantity,upc)
-	values (011,'consession',null,0.99,'Can of Coke',158,049000028904);
-insert into Item (itemID,item_type,description,price,name,quantity,upc)
-	values (012,'consession',null,0.99,'Can of Pepsi',266,012000018794);
-insert into Item (itemID,item_type,description,price,name,quantity,upc)
-	values (013,'funko pop','Funko pop figurine',11.99,'Star Wars Rey',10,889698117081);
-insert into Item (itemID,item_type,description,price,name,quantity,upc)
-	values (014,'funko pop','Funko pop figurine',29.00,'Marvel Captian America w/Photon shield',6,889698101288);
-insert into Item (itemID,item_type,description,price,name,quantity,upc)
-	values (015,'funko pop','Funko pop figurine',8.49,'Marvel Dr. Strange Kaecilius',3,889698101837);
-insert into Item (itemID,item_type,description,price,name,quantity,upc)
-	values (016,'funko pop','Funko pop figurine',21.99,'Star Wars Dagobah Yoda',5,889698101059);
-insert into Item (itemID,item_type,description,price,name,quantity,upc)
-	values (017,'consession',null,1.50,'Chocolate muffin',18,025896314745);
-insert into Item (itemID,item_type,description,price,name,quantity,upc)
-	values (018,'consession',null,1.50,'Blueberry muffin',22,025896314746);
-insert into Item (itemID,item_type,description,price,name,quantity,upc)
-	values (019,'consession',null,1.25,'Potato chips - plain',20,005789654885);
-insert into Item (itemID,item_type,description,price,name,quantity,upc)
-	values (020,'consession',null,1.25,'Potato chips - Salt and vinegar',20,005789654886);
-insert into Item (itemID,item_type,description,price,name,quantity,upc)
-	values (021,'act figure','Marvel Action figure',12.75,'Venom action figure',5,457896321554);
-insert into Item (itemID,item_type,description,price,name,quantity,upc)
-	values (022,'act figure','Marvel Action figure',12.75,'Captian America action figure',2,457896321578);
-insert into Item (itemID,item_type,description,price,name,quantity,upc)
-	values (023,'act figure','DC Action figure',11.25,'Superman action figure',5,951753852456);
-insert into Item (itemID,item_type,description,price,name,quantity,upc)
-	values (024,'act figure','DC Action figure',11.25,'Flash action figure',5,173846258425);
+insert into Item (itemID, item_type, vendorID, description, price, name, quantity, upc)
+	values (001, 'comic', 1, null, 15.99, 'Batman: The Killing Joke', 5, 82341785611092745);
+insert into Item (itemID, item_type, vendorID, description, price, name, quantity, upc)
+	values (002, 'comic', 1, 'King Thor volume 1', 10.99, 'King Thor', 10, 32904867028978325);
+insert into Item (itemID, item_type, vendorID, description, price, name, quantity, upc)
+	values (003, 'comic', 2 , 'Once and Future volume 1', 16.99, 'Once and Future',3,094785619542856347 );
+insert into Item (itemID, item_type, vendorID, description, price, name, quantity, upc)
+	values (004, 'comic', 2 , 'Killadelphia Volume 1: Sins of the Father', 9.99, 'Killadelphia',8, 96874325412358974 );
+insert into Item (itemID, item_type, vendorID, description, price, name, quantity, upc)
+	values (005, 'comic', 1, 'Batman: Last Knight on Earth Hardcover', 39.59 , 'Batman: Last Knight on Earth',2,788552147963258 );
+insert into Item (itemID, item_type, vendorID, description, price, name, quantity, upc)
+	values (006, 'comic',1, 'DCeased Hardcover',34.15, 'DCeased',4, 96874325415438974);
+insert into Item (itemID, item_type, vendorID, description, price, name, quantity, upc)
+	values (007, 'comic',1,'Age of X-Man: X-Tremists compilation',19.17 ,'Age of X-Man: X-Tremists',1,45887741258963014);
+insert into Item (itemID, item_type, vendorID, description, price, name, quantity, upc)
+	values (008, 'comic',1,'Green Lantern: Earth One Vol. 2 Hardcover',33.65,'Green Lantern: Earth One Vol. 2',2,85236974132145698);
+insert into Item (itemID, item_type, vendorID, description, price, name, quantity, upc)
+	values (009, 'comic',1,'Immortal Hulk Vol. 5: Breaker of Worlds',19.18,'Immortal Hulk Vol. 5',6,96374185245695175);
+insert into Item (itemID, item_type, vendorID, description, price, name, quantity, upc)
+	values (010,'comic',1,'FN Spider-Man Complete',39.64,'Friendly Neighborhood Spider-Man',7,5236478910304 );
+insert into Item (itemID,item_type,vendorID,description,price,name,quantity,upc)
+	values (011,'consession',3,null,0.99,'Can of Coke',158,049000028904);
+insert into Item (itemID,item_type,vendorID,description,price,name,quantity,upc)
+	values (012,'consession',4,null,0.99,'Can of Pepsi',266,012000018794);
+insert into Item (itemID,item_type,vendorID,description,price,name,quantity,upc)
+	values (013,'funko pop',5,'Funko pop figurine',11.99,'Star Wars Rey',10,889698117081);
+insert into Item (itemID,item_type,vendorID,description,price,name,quantity,upc)
+	values (014,'funko pop',5,'Funko pop figurine',29.00,'Marvel Captian America w/Photon shield',6,889698101288);
+insert into Item (itemID,item_type,vendorID,description,price,name,quantity,upc)
+	values (015,'funko pop',5,'Funko pop figurine',8.49,'Marvel Dr. Strange Kaecilius',3,889698101837);
+insert into Item (itemID,item_type,vendorID,description,price,name,quantity,upc)
+	values (016,'funko pop',5,'Funko pop figurine',21.99,'Star Wars Dagobah Yoda',5,889698101059);
+insert into Item (itemID,item_type,vendorID,description,price,name,quantity,upc)
+	values (017,'consession',6,null,1.50,'Chocolate muffin',18,025896314745);
+insert into Item (itemID,item_type,vendorID,description,price,name,quantity,upc)
+	values (018,'consession',6,null,1.50,'Blueberry muffin',22,025896314746);
+insert into Item (itemID,item_type,vendorID,description,price,name,quantity,upc)
+	values (019,'consession',6,null,1.25,'Potato chips - plain',20,005789654885);
+insert into Item (itemID,item_type,vendorID,description,price,name,quantity,upc)
+	values (020,'consession',6,null,1.25,'Potato chips - Salt and vinegar',20,005789654886);
+insert into Item (itemID,item_type,vendorID,description,price,name,quantity,upc)
+	values (021,'act figure',7,'Marvel Action figure',12.75,'Venom action figure',5,457896321554);
+insert into Item (itemID,item_type,vendorID,description,price,name,quantity,upc)
+	values (022,'act figure',7,'Marvel Action figure',12.75,'Captian America action figure',2,457896321578);
+insert into Item (itemID,item_type,vendorID,description,price,name,quantity,upc)
+	values (023,'act figures',8,'DC Action figure',11.25,'Superman action figure',5,951753852456);
+insert into Item (itemID,item_type,vendorID,description,price,name,quantity,upc)
+	values (024,'act figure',8,'DC Action figure',11.25,'Flash action figure',5,173846258425);
+			
--- Database/Load_Transaction.sql
+++ Database/Load_Transaction.sql
@@ -1,29 +1,16 @@
 /*insert into Transaction (transactionID,transaction_date)
 	values (00,YYYY-MM-DD HH:MI:SS);*/
 insert into Transaction (transactionID,transaction_date)
-	values (01,STR_TO_DATE('2020,01,19 10,27,28', '%Y,%m,%d %h,%i,%s'));
+	values (01,2020-01-19 10:27:28);
 insert into Transaction (transactionID,transaction_date)
-	values (02,STR_TO_DATE('2020,01,19 10,45,30', '%Y,%m,%d %h,%i,%s'));
+	values (02,2020-01-19 10:45:30);
 insert into Transaction (transactionID,transaction_date)
-	values (03,STR_TO_DATE('2020,01,19 12,18,06', '%Y,%m,%d %h,%i,%s'));
+	values (03,2020-01-19 12:18:06);
 insert into Transaction (transactionID,transaction_date)
-	values (04,STR_TO_DATE('2020,01,20 7,48,20', '%Y,%m,%d %h,%i,%s'));
+	values (04,2020-01-20 7:48:20);
 insert into Transaction (transactionID,transaction_date)
-	values (05,STR_TO_DATE('2020,01,21 9,22,18', '%Y,%m,%d %h,%i,%s'));
+	values (05,2020-01-21 9:22:18);
 insert into Transaction (transactionID,transaction_date)
-	values (06,STR_TO_DATE('2020,01,23 9,55,27', '%Y,%m,%d %h,%i,%s'));
+	values (06,2020-01-23 9:55:27);
 insert into Transaction (transactionID,transaction_date)
-	values (07,STR_TO_DATE('2020,01,25 11,15,27', '%Y,%m,%d %h,%i,%s'));							
-	values (01,STR_TO_DATE('2020-01-19 10-27-28','%Y-%m-%d %h-%i-%s'));
-insert into Transaction (transactionID,transaction_date)
-	values (02,STR_TO_DATE('2020-01-19 10:45:30','%Y-%m-%d %h:%i:%s'));
-insert into Transaction (transactionID,transaction_date)
-	values (03,STR_TO_DATE('2020-01-19 12:18:06','%Y-%m-%d %h:%i:%s'));
-insert into Transaction (transactionID,transaction_date)
-	values (04,STR_TO_DATE('2020-01-20 7:48:20','%Y-%m-%d %h:%i:%s'));
-insert into Transaction (transactionID,transaction_date)
-	values (05,STR_TO_DATE('2020-01-21 9:22:18','%Y-%m-%d %h:%i:%s'));
-insert into Transaction (transactionID,transaction_date)
-	values (06,STR_TO_DATE('2020-01-23 9:55:27','%Y-%m-%d %h:%i:%s'));
-insert into Transaction (transactionID,transaction_date)
-	values (07,STR_TO_DATE('2020-01-25 11:15:27','%Y-%m-%d %h:%i:%s'));						
+	values (07,2020-01-25 11:15:27);							
