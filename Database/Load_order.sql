1c63d7228aa2d79b789b9fba5b9fca6504377676
skruey <sskrudland@hotmail.com>
2020-03-31 13:58:57 -0600

    Working Changes for DB

--- Database/Load_order.sql
+++ Database/Load_order.sql
@@ -1,19 +1,18 @@
-/*insert into Order (order_no)
-	values (0000);*/
-insert into Order (order_no)
-	values (0001);
-insert into Order (order_no)
-	values (0002);
-insert into Order (order_no)
-	values (0003);
-insert into Order (order_no)
-	values (0004);
-insert into Order (order_no)
-	values (0005);
-insert into Order (order_no)
-	values (0006);
-insert into Order (order_no)
-	values (0007);
-insert into Order (order_no)
-	values (0008);
-									
+/*insert into Orders (order_no)
+	values (0000,2020-01-19);*/
+insert into Orders (order_no, orderDate, arrivalDate, VendorID)
+	values (0001,STR_TO_DATE('2019-12-10','%Y-%m-%d'),STR_TO_DATE('2020-01-10', '%Y-%m-%d'), 1);
+insert into Orders (order_no, orderDate, arrivalDate, VendorID)
+	values (0002,STR_TO_DATE('2019-12-10','%Y-%m-%d'),STR_TO_DATE('2019-01-10', '%Y-%m-%d'), 2);
+insert into Orders (order_no, orderDate, arrivalDate, VendorID)
+	values (0003,STR_TO_DATE('2019-12-08','%Y-%m-%d'),STR_TO_DATE('2019-01-12', '%Y-%m-%d'), 3);
+insert into Orders (order_no, orderDate, arrivalDate, VendorID)
+	values (0004,STR_TO_DATE('2019-12-07','%Y-%m-%d'),STR_TO_DATE('2019-01-12', '%Y-%m-%d'), 4);
+insert into Orders (order_no, orderDate, arrivalDate, VendorID)
+	values (0005,STR_TO_DATE('2019-12-05','%Y-%m-%d'),STR_TO_DATE('2019-01-12', '%Y-%m-%d'), 5);
+insert into Orders (order_no, orderDate, arrivalDate, VendorID)
+	values (0006,STR_TO_DATE('2019-12-05','%Y-%m-%d'),STR_TO_DATE('2019-01-15', '%Y-%m-%d'), 6);
+insert into Orders (order_no, orderDate, arrivalDate, VendorID)
+	values (0007,STR_TO_DATE('2019-12-03','%Y-%m-%d'),STR_TO_DATE('2019-01-15', '%Y-%m-%d'), 7);
+insert into Orders (order_no, orderDate, arrivalDate, VendorID)
+	values (0008,STR_TO_DATE('2019-12-01','%Y-%m-%d'),STR_TO_DATE('2019-01-15', '%Y-%m-%d'), 8);
--- Database/RUN.sql
+++ Database/RUN.sql
@@ -1,1 +1,31 @@
-source 'CAPSTONE_TABLE_CREATION.SQL';
+-- DON'T FORGET TO CHANGE THE PATH!
+
+select 'Creation Table' as '';
+source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/CAPSTONE_TABLE_CREATION.SQL;
+ 
+select 'Loading Vendors' as '';
+source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/Load_vendor.SQL;
+
+select 'Loading Type' as ''; 
+source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/Load_type.SQL;
+
+select 'Loading Order' as '';
+source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/Load_order.SQL;
+
+select 'Loading Refund' as '';
+source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/Load_refund.SQL;
+
+select 'Loading Transaction' as '';
+source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/Load_Transaction.SQL;
+
+select 'Loading Item' as '';
+source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/Load_Item.SQL;
+
+select 'Loading Comic' as '';
+source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/Load_comic.SQL;
+
+select 'Loading OrderItem' as '';
+source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/Load_OrderItem.SQL;
+
+select 'Loading TransactionItem' as '';
+source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/Load_transactionItem.SQL;
