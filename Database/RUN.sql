305f5a79d6747b2bd7fcb03f4c77cdc0b9a3f55f
skruey <sskrudland@hotmail.com>
2020-03-31 14:05:30 -0600

    Update my run file

--- Database/RUN.sql
+++ Database/RUN.sql
@@ -1,31 +1,31 @@
 -- DON'T FORGET TO CHANGE THE PATH!
 
 select 'Creation Table' as '';
-source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/CAPSTONE_TABLE_CREATION.SQL;
+source C:/Users/749295/Documents/GitHub/Capstone-Project/Database/CAPSTONE_TABLE_CREATION.SQL;
  
 select 'Loading Vendors' as '';
-source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/Load_vendor.SQL;
+source C:/Users/749295/Documents/GitHub/Capstone-Project/Database/Load_vendor.SQL;
 
 select 'Loading Type' as ''; 
-source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/Load_type.SQL;
+source C:/Users/749295/Documents/GitHub/Capstone-Project/Database/Load_type.SQL;
 
 select 'Loading Order' as '';
-source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/Load_order.SQL;
+source C:/Users/749295/Documents/GitHub/Capstone-Project/Database/Load_order.SQL;
 
 select 'Loading Refund' as '';
-source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/Load_refund.SQL;
+source C:/Users/749295/Documents/GitHub/Capstone-Project/Database/Load_refund.SQL;
 
 select 'Loading Transaction' as '';
-source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/Load_Transaction.SQL;
+source C:/Users/749295/Documents/GitHub/Capstone-Project/Database/Load_Transaction.SQL;
 
 select 'Loading Item' as '';
-source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/Load_Item.SQL;
+source C:/Users/749295/Documents/GitHub/Capstone-Project/Database/Load_Item.SQL;
 
 select 'Loading Comic' as '';
-source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/Load_comic.SQL;
+source C:/Users/749295/Documents/GitHub/Capstone-Project/Database/Load_comic.SQL;
 
 select 'Loading OrderItem' as '';
-source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/Load_OrderItem.SQL;
+source C:/Users/749295/Documents/GitHub/Capstone-Project/Database/Load_OrderItem.SQL;
 
 select 'Loading TransactionItem' as '';
-source C:/Users/Vinicius Smith/Google Drive/SAIT/_IT2/CPRG-250-Database/Project/Capstone-Project/Database/Load_transactionItem.SQL;
+source C:/Users/749295/Documents/GitHub/Capstone-Project/Database/Load_transactionItem.SQL;
